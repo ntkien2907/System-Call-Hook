fe53f86c2777c2eb5a1e6a5c43c4361e527fda84
ntkien2907 <j13daian@gmail.com>
2020-05-20 01:04:52 +0700

    pnametoid

---
+++ run_pnametoid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	char data[16];
+	printf("Enter your process's name: ");
+	scanf("%s", data);
+	while((getchar()) != '\n');
+
+	long int pid = syscall(436, data, 16);
+	printf("System call returns %ld\n", pid); 
+       	return 0;
+}
