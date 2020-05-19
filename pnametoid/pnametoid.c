f3f8521b4cd1d66209583f71ef8d4e2e74f82c7e
ntkien2907 <j13daian@gmail.com>
2020-05-20 01:02:56 +0700

    pnametoid

--- pnametoid/pnametoid.c
+++ pnametoid/pnametoid.c
@@ -3,19 +3,24 @@
 #include <linux/sched.h>
 #include <linux/init.h>
 #include <linux/string.h>
+#include <linux/uaccess.h>
 
-asmlinkage long sys_pnametoid (char* name) 
+SYSCALL_DEFINE2(pnametoid, char __user *, data, int, len)
 {
 	/* task_struct la mot cau truc du lieu, chua cac thong tin cua process */
 	struct task_struct *task;
 
+	char buf[16];
+	if(copy_from_user(buf, data, len))
+		return -1;
+	
 	/* duyet qua tat ca cac process cua he dieu hanh */
 	for_each_process(task) 
 	{
 		/* task->comm la mot mang char luu ten cua process 
 		   so sanh ten process nhap vo (name) voi task->comm */
-		printk("%s\t %s\n", task->comm, name);
-		if(strcmp(task->comm, name) == 0) 
+		printk("%s", task->comm);
+		if(strcmp(task->comm, buf) == 0) 
 		{
 			/* task_pid_nr(task) tra ve "global" PIDs cua process hien 
 			tai va con so nay la duy nhat trong toan bo he dieu hanh */
